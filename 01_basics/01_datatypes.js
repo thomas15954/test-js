a3bbb72ea3359453d92b52c7f36be4e782525325
thomas15954 <thomaswalker15954@gmail.com>
2025-07-09 17:10:01 +0000

    container config done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; // treat all js code as new version
+
+//alert(3+3) // we are using node js, not browser
+
+console.log(3+3)
+
+console.log("waqar")
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 144553
+let accountEmail = "thomas@gmail.com"
+var accountPassword = "12345"
+accountCity = "karachi"
+let accountState;
+
+// accountID = 12// not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "456"
+accountCity = "lahore"
+
+console.log(accountID);
+console.table ([accountID, accountEmail, accountPassword, accountCity, accountState])
