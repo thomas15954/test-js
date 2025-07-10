7d20b638af0ba7bfde90ccaf96a87b7b5853973c
thomas15954 <thomaswalker15954@gmail.com>
2025-07-10 17:46:10 +0000

    datatypes

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -2,6 +2,26 @@
 
 //alert(3+3) // we are using node js, not browser
 
-console.log(3+3)
+console.log(3+3) // code must be readable
 
 console.log("waqar")
+
+let name = "waqar"
+let age = 22
+let isloggedin = false
+
+// number => 2 to power  53
+// bigint
+// string => "" prefer to use in ""
+// boolean => true/false use for yes or no condition
+// null => standalone value
+// undefined => value not assigned
+//symbols => unique
+
+
+// object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
