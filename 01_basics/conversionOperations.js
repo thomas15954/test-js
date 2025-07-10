1045207ef24c3604767835131538954ded352ec6
thomas15954 <thomaswalker15954@gmail.com>
2025-07-10 18:11:52 +0000

    conversionOperations

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "waqar"
+
+//console.log(typeof (score));
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1
+// false => 0
+
+
+let isloggedIn = "waqar"
+
+let booleanisloggedIn = Boolean(isloggedIn)
+//console.log(booleanisloggedIn);
+
+// 1 =>  true; 0 => false
+// "" => false
+// "waqar" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
