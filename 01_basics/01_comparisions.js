7b7001879a38f13fb375b06b6b1845d2ee4963ba
thomas15954 <thomaswalker15954@gmail.com>
2025-07-10 22:55:18 +0000

    comparisions

---
+++ 01_basics/01_comparisions.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log( "2" > 1);
+// console.log( "02" > 1);
+
+// console.log(null > 0); // not prefer to use these type of comparision, avoid this form line 8-13//
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined >0);
+// console.log(undefined < 0);
+
+// strict comparision
+
+console.log("2" === 2);
